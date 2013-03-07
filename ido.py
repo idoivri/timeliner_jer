31a4c780f175c9e2f5ab9d800def96cd17055351
ido ivri <idoivri@gmail.com>
2013-03-07 05:11:17 +0200

    added stuff to ido.ty

--- ido.py
+++ ido.py
@@ -1,9 +1,37 @@
 import requests
+from bs4 import BeautifulSoup
 
-r = requests.get('http://www.jnul.huji.ac.il/dl/maps/jer/html/date.html')
+try:
+	r = requests.get('http://www.jnul.huji.ac.il/dl/maps/jer/html/date.html')
 
-print r.status_code
+
+	print r.status_code
+
+	if r.status_code != 200:
+		raise Exception("Status code: %s" % r.status_code)
+
+except Exception, e:
+		print "ERROR: %s" % e
+		exit()
+
+
 
 print '===='
 
 print r.headers['content-type']
+
+print '===='
+
+print r.url
+
+print '===='
+
+#print r.text
+
+soup = BeautifulSoup(r.text)
+
+
+print '===='
+
+
+
